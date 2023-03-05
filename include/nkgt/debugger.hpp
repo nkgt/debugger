1963453119f90a7bb2a962ba39a5634e0917402d
Nicolò Ginatta <nicolo.ginatta@gmail.com>
2023-03-05 22:48:13 +0900

    Adds breakpoint logic

--- include/nkgt/debugger.hpp
+++ include/nkgt/debugger.hpp
@@ -1,7 +1,18 @@
 #pragma once
+#include <cstdint>
 #include <sys/types.h>
 
 namespace nkgt::debugger {
+
+struct breakpoint {
+    pid_t pid;
+    std::intptr_t address;
+    bool enabled;
+    uint8_t saved_data;
+};
+
+void enable_brakpoint(breakpoint& bp);
+void disable_brakpoint(breakpoint& bp);
 
 void run(pid_t pid);
 
--- src/debugger.cpp
+++ src/debugger.cpp
@@ -1,6 +1,7 @@
 #include "nkgt/debugger.hpp"
 #include "nkgt/util.hpp"
 
+#include <cerrno>
 #include <linenoise.h>
 #include <fmt/core.h>
 
@@ -31,6 +32,55 @@ static void handle_command(const std::string& line, pid_t pid) {
     } else {
         fmt::print("Unknow command");
     }
+}
+
+// In x86 0xcc is the instruction that identifies a breakpoint. So enabling
+// a breakpoint simply means replacing whatever instruction is at
+// bp.address with 0xcc.
+void enable_brakpoint(breakpoint& bp) {
+    // Calling ptrace with PTRACE_PEEKDATA retrieves the word at bp.address for
+    // the process identified by bp.pid. This means that -1 is a valid return
+    // value and does not necesserely describe an error. To solve this we clear
+    // errno before calling ptrace and we check it right after.
+    // More info at RETURN_VALUE in ptrace(2).
+    errno = 0;
+    long data = ptrace(PTRACE_PEEKDATA, bp.pid, bp.address, nullptr);
+    if(data == -1 && errno != 0) {
+        util::print_error_message("ptrace", errno);
+        return;
+    }
+
+
+    long data_with_trap = ((data & ~0xff) | 0xcc);
+    bp.saved_data = static_cast<uint8_t>(data & 0xff);
+    
+    if(ptrace(PTRACE_POKEDATA, bp.pid, bp.address, data_with_trap) == -1) {
+        util::print_error_message("ptrace", errno);
+        return;
+    }
+
+    bp.enabled = true;
+}
+
+// Disables a breakpoint by restoring the original data (bp.saved_data) in the
+// location bp.address.
+void disable_brakpoint(breakpoint& bp) {
+    // See comment in enable_brakpoint() for more info.
+    errno = 0;
+    long data = ptrace(PTRACE_PEEKDATA, bp.pid, bp.address, nullptr);
+    if(data == -1 && errno != 0) {
+        util::print_error_message("ptrace", errno);
+        return;
+    }
+
+    long restored_data = ((data & ~0xff) | bp.saved_data);
+
+    if(ptrace(PTRACE_POKEDATA, bp.pid, bp.address, restored_data) == -1) {
+        util::print_error_message("ptrace", errno);
+        return;
+    }
+
+    bp.enabled = false;
 }
 
 void run(pid_t pid) {
