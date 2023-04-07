e453fa2ea7167512d0690fe14bb1e42a6dc1b1fb
Nicolò Ginatta <nicolo.ginatta@gmail.com>
2023-04-07 23:53:51 +0900

    feat: better input file validation

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -1,6 +1,6 @@
 #include <filesystem>
 namespace fs = std::filesystem;
-
+#include <fstream>
 #include <sys/personality.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
@@ -29,6 +29,36 @@ static void execute_debugee(const char* program_name) {
     }
 }
 
+// Basic check for the input file validity.
+// Just for safety, we accept only ELF executable as debugee. As per elf(3), a
+// ELF executable will start with a magic nubmer formed by the bytes 0x7f, 'E',
+// 'L', 'F'. So we just load the first 4 bytes and check them.
+static bool is_file_valid(const fs::path& program_path) {
+    const bool condition = fs::exists(program_path) && 
+                           fs::is_regular_file(program_path) &&
+                           fs::file_size(program_path) >= 4;
+
+    if(condition) {
+        std::ifstream executable(program_path);
+
+        if(!executable) {
+            fmt::print("Failed to open file {}.\n", program_path);
+            return false;
+        }
+
+        char magic_number[4];
+        executable.read(magic_number, 4);
+        executable.close();
+
+        return magic_number[0] == 0x7f &&
+               magic_number[1] == 'E'  &&
+               magic_number[2] == 'L'  &&
+               magic_number[3] == 'F';
+    }
+
+    return false;
+}
+
 int main(int argc, const char** argv) {
     if(argc < 2) {
         fmt::print("Program name not specified!");
@@ -36,7 +66,7 @@ int main(int argc, const char** argv) {
     }
 
     const fs::path program_path(argv[1]);
-    if(!fs::exists(program_path) || !fs::is_regular_file(program_path)) {
+    if(!is_file_valid(program_path)) {
         fmt::print("The file {} does not exists or is not a regular file.\n", program_path);
         return EXIT_FAILURE;
     }
